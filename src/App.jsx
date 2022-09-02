3fc3ef50cc7c5d747c8db6eadc52e8b804ec7230
hisham saymeh <hishamss90@gmail.com>
2022-09-01 22:31:05 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>ReportsV7</div>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
+export default App;
